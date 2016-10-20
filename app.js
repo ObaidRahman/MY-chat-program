399b25e405428b590cc5dc4467402fb295e55022
MOHAMMED OBAID UR RAHMAN <obaidrahman49@gmail.com>
2016-10-20 17:19:53 -0400

    A Simple chat room
    A simple chat room using NodeJS, Socket IO, Express and Mongodb

---
+++ app.js
@@ -0,0 +1,126 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var socket_io = require('socket.io'),
+    userNames = [];
+var MongoClient = require('mongoose');
+//var mongoose = require('mongoose');
+
+var app = express();
+
+var io = socket_io();
+app.io = io;
+
+MongoClient.connect('mongodb://localhost/Chat', function(err,db) {
+    if(err){
+        console.log(err);
+    }else
+    {
+        console.log("Connected to MongoDB !");
+    }
+
+});
+
+var chatCollection = new MongoClient.Schema({
+   person: String,
+    msg: String
+});
+
+var Chat = MongoClient.model('Message', chatCollection);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/clientchat.html');
+});
+
+io.sockets.on('connection', function(socket){
+
+    Chat.find({}, function(err, data){
+        if(err){
+            throw err;
+        }
+        socket.emit('history logs', data);
+    });
+
+  socket.on('new users', function(data, callback){
+    if(userNames.indexOf(data)!== -1){
+      callback(false);
+    } else{
+
+      callback(true);
+      socket.username = data;
+      userNames.push(socket.username);
+      io.sockets.emit('allusers', userNames);
+
+    }
+  });
+  socket.on('message', function(data){
+
+     var savedChat = new Chat({msg: data, person: socket.username});
+      savedChat.save(function(err){
+          if(err){
+              throw err;
+          }
+
+      });
+      io.sockets.emit('new message',{msg: data, person: socket.username} );
+
+
+  });
+
+    socket.on('disconnect', function(data){
+
+       if(!socket.username) {return;}
+
+    });
+});
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
